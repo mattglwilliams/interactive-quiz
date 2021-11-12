1b904b8b4c408d14ee095bc923d300e29ceb8a37
Matthew Williams <matthewwilliams@matts-mbp.home>
2021-11-12 22:27:40 +0000

    Added function to go back to the quiz and to clear the highscores

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -2,3 +2,13 @@ var mainContainer = document.getElementById('questions')
 var scoreLog = document.getElementById('score-log')
 var clearButton = document.getElementById('clear-btn')
 var backButton = document.getElementById('back-btn')
+
+
+
+clearButton.addEventListener("click", function() {
+    scoreLog.innerHTML = ""
+})
+
+backButton.addEventListener("click", function() {
+    window.location.replace("./index.html");
+})
