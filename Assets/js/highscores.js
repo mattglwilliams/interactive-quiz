f4e9ad49475b6200e9dcc718bb370a64e90284dd
Matthew Williams <matthewwilliams@matts-mbp.home>
2021-11-12 22:24:09 +0000

    Added dom elements to the highscores js

---
+++ Assets/js/highscores.js
@@ -0,0 +1,4 @@
+var mainContainer = document.getElementById('questions')
+var scoreLog = document.getElementById('score-log')
+var clearButton = document.getElementById('clear-btn')
+var backButton = document.getElementById('back-btn')
