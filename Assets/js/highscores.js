11acb540a07570241dad27b82a92f80c6d384fa8
Matthew Williams <matthewwilliams@matts-mbp.home>
2021-11-12 22:28:45 +0000

    Got my scores from local storage and added function to add them to the page

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -3,7 +3,15 @@ var scoreLog = document.getElementById('score-log')
 var clearButton = document.getElementById('clear-btn')
 var backButton = document.getElementById('back-btn')
 
+var finalScoreEntry = localStorage.getItem('scores')
 
+if (finalScoreEntry !== null) {
+    for (var i = 0; i < finalScoreEntry.length; i++) {
+        var userEntryLi = document.createElement('li');
+        userEntryLi.textContent = finalScoreEntry;
+        scoreLog.appendChild(userEntryLi);
+    }
+}
 
 clearButton.addEventListener("click", function() {
     scoreLog.innerHTML = ""
