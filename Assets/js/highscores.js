3073c0d223366066e177f2c9aaa0137d9b317f68
Matthew Williams <matthewwilliams@matts-mbp.home>
2021-11-13 20:02:19 +0000

    Added comments to each document for readability purposes

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,24 +1,33 @@
+// setting the DOM elements
 var mainContainer = document.getElementById('questions')
 var scoreLog = document.getElementById('score-log')
 var clearButton = document.getElementById('clear-btn')
 var backButton = document.getElementById('back-btn')
 
+// getting the initials and scores from local storage
 let overallScore = JSON.parse(localStorage.getItem('scores'));
 
+// if there are initials and scores stored locally, then...
 if (overallScore !== null) {
 
+    // looping through the locally stores initials and scores and creating a list item for each
     for (let i = 0; i < overallScore.length; i++) {
         var userEntryLi = document.createElement('li');
         userEntryLi.textContent = overallScore[i].user + " " + overallScore[i].score;
+        // appenging the newly created list items to the scores UL
         scoreLog.appendChild(userEntryLi);
     }
 }
 
+// event listener button to reset the highscores
 clearButton.addEventListener("click", function() {
+    // clears all the HTML in the scores UL
     scoreLog.innerHTML = ""
+    // clears the local storage
     localStorage.clear();
 })
 
+// event listener button to take the user back to the quiz
 backButton.addEventListener("click", function() {
     window.location.replace("./index.html");
 })
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,4 @@
+// setting the quiz questions as objects within an array
 let questions = [
     {
         question: 'Inside which HTML element do we put the JavaScript?',
@@ -51,18 +52,25 @@ let questions = [
     },
 ];
 
+// setting the DOM elements
 const timer = document.getElementById('timer');
 const questionContainer = document.getElementById('questions-container');
 const questionsElement = document.getElementById('questions');
 const startBtn = document.getElementById('start-btn');
 
+// setting the global variables
+// this is to store the users score. They get 1 point per correct answer
 let userScore = 0;
+// this is the starting time
 let timeLeft = 50;
+// this is the amount of time knocked off for an incorrect answer
 let timePenalty = 5;
+// this is the index the questions will start on
 let currentQuestionIndex = 0;
-let scores = [];
 
-startBtn.addEventListener("click", function runTimer () {
+// event listener on the start button
+startBtn.addEventListener("click", function runTimer() {
+    // function to start the timer, to reduce it by 1000 miliseconds
     let timerInterval = setInterval(function () {
         if (timeLeft > 0) {
             timeLeft--;
@@ -70,91 +78,133 @@ startBtn.addEventListener("click", function runTimer () {
         } else {
             clearInterval(timerInterval);
             timer.textContent = "Times up"
+            // once the time is up, run the endOfGame function
             endOfGame();
         }
     }, 1000);
+    // when the start button is clicked, clear the questions container inner HTML and ...
     questionsElement.innerHTML = "";
+    // ... run the getQuestions function
     getQuestions();
 });
 
-function getQuestions () {
+// this function will get and display the relevent question
+function getQuestions() {
     for (let i = 0; i < questions.length; i++) {
+        // variable to hold the question of the current index in the array
         let displayQuestion = questions[currentQuestionIndex].question;
+        // variable to hold the choices of the current index in the array
         let displayChoices = questions[currentQuestionIndex].choices;
+        // adding the current question to the question container
         questionsElement.textContent = displayQuestion;
+        // creating a ul to hold the choices list
         let choicesUl = document.createElement('ul');
-    displayChoices.forEach(function (choice) {
-        let choicesLi = document.createElement('li')
+        // a for each function to create an li for each choice
+        displayChoices.forEach(function (choice) {
+            let choicesLi = document.createElement('li')
             choicesLi.textContent = choice;
+            // appending each li to the ul
             choicesUl.appendChild(choicesLi)
+            // adding an event listener on each li, which on click, will run the checkAnswer function
             choicesLi.addEventListener('click', checkAnswer)
         })
-            questionsElement.appendChild(choicesUl)
+        // appending the ul that holds the choices list to the questions container
+        questionsElement.appendChild(choicesUl)
     }
 }
 
+// function to check the choice and if it is correct or incorrect
 function checkAnswer(event) {
     let selectedAnswer = event.target
+    // creating a new p tag that will tell the user if their chosen answer is correct or incorrect
     let answerMessage = document.createElement("p")
+    // giving the new p tag a class so we can style it
     answerMessage.setAttribute("class", "answer-message")
+    // if the answer they selected is correct then...
     if (selectedAnswer.textContent === questions[currentQuestionIndex].answer) {
         console.log("Correct");
+        // add a point to their socre and ...
         userScore++;
+        // ... let them know it was correct
         answerMessage.textContent = "Correct!"
+        // however, if it was wrong then...
     } else {
         console.log("Incorrect")
+        // reduce the time left by 5 seconds and ...
         timeLeft = timeLeft - timePenalty;
+        // ... let them know it was incorrect
         answerMessage.textContent = "Incorrect!"
     }
 
+    // once they have selected a choice, move onto the next object(question) in the array
     currentQuestionIndex++
 
+    // if we have no more questions left, run the endOfGameFunction, however ...
     if (currentQuestionIndex >= questions.length) {
         endOfGame();
+        // ... if we do still have questions left, get the next question
     } else {
         getQuestions();
     }
+    // appending the message that tells them if their choice was correct or incorrect, to the questions container
     questionsElement.appendChild(answerMessage)
-  }
+}
 
-  function endOfGame() {
+// function to run when either they have answered all questions or the time is up
+function endOfGame() {
+    // firstly, clear the questions container elements
     questionsElement.innerHTML = ""
     timeLeft = 0;
+    // creating all the new elements needed to let them know the game has ended, what score theyy got, and
+    // allow them to log their highscore
     let gameOverHeading = document.createElement("h1")
     let gameOverContent = document.createElement("p")
     let initialsLabel = document.createElement("label")
     let initialsInput = document.createElement("input")
     let initialsSubmitButton = document.createElement("button")
-    // initialsSubmit.setAttribute("type", "submit")
+
+    // setting the text of the newly created elements
     gameOverHeading.textContent = "Game Over!"
     gameOverContent.textContent = "You scored " + userScore + "."
     initialsLabel.textContent = " Enter your initials to save your score: "
     initialsInput.textContent = ""
     initialsSubmitButton.textContent = "Save your score"
+
+    // appending the new elements to the questions container
     questionsElement.appendChild(gameOverHeading);
     questionsElement.appendChild(gameOverContent);
     questionsElement.appendChild(initialsLabel);
     questionsElement.appendChild(initialsInput);
     questionsElement.appendChild(initialsSubmitButton);
 
+    // event listener on the submit button
     initialsSubmitButton.addEventListener("click", function (event) {
+        // ffirstly, prevent the default behaivour
         event.preventDefault();
+        // variable to get the initials that the user entered into the initials input
         let initials = initialsInput.value;
 
+        // an object to store both their initials and their score
         let initialsAndScore = {
             user: initials,
             score: userScore
         }
 
-        if(localStorage.getItem('scores') === null){
+        // setting the local array that will hold each entry
+        if (localStorage.getItem('scores') === null) {
             localStorage.setItem('scores', '[]');
         }
 
+        // converting the json string into a js object the browser can read
         let overallScore = JSON.parse(localStorage.getItem('scores'));
+
+        // pushing the objects to the overallScore variable
         overallScore.push(initialsAndScore);
 
+        // setting the loca storage item
         localStorage.setItem('scores', JSON.stringify(overallScore));
 
+        // once they have submitted, they're taken to the highscores page
         window.location.replace("./HighScores.html");
     });
-  };
+};
